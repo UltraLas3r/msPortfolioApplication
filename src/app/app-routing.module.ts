4d2311645bd487795f7c35e84b81815ecaede326
Michael Schreiber <mschreiber@its4logistics.com>
2023-11-29 10:56:32 -0800

    addded more routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
 
-const routes: Routes = [{path: 'home', component: HomeComponent}];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'portfolio', component: PortfolioComponent}, 
+  {path: 'resume', component: ResumeComponent},
+  {path: 'Contact', component: ContactComponent} ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
