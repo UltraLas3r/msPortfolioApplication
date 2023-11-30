746167902d347cbbe62c4c681560c44b2381c237
Michael Schreiber <mschreiber@its4logistics.com>
2023-11-30 13:33:53 -0800

    finished basic outline of home and started portfolio

---
+++ src/app/projectcard/projectcard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projectcard',
+  templateUrl: './projectcard.component.html',
+  styleUrls: ['./projectcard.component.css']
+})
+export class ProjectcardComponent {
+
+}
