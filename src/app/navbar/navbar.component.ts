60357fe26e2c9c8d8fe2a1c81da97d6aee45b854
Michael Schreiber <michaelpschreiber@gmail.com>
2023-12-03 08:36:07 -0800

    working on highlighting navbar elements

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  selectedNavBarItem: string | null = null;
+
+  selectedItem(navbarItemName: string){
+    this.selectedNavBarItem = navbarItemName;
+  }
 
 }
