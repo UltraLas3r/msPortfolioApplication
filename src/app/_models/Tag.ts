430420f708db3c98d35ba07b7b1063cc87bc0e06
Michael Schreiber <mschreiber@its4logistics.com>
2023-12-07 07:36:00 -0800

    added colors and more sample projects

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -1,13 +1,19 @@
 export class Tag{
     //change the color choices to personalize this later on
-    static readonly ANGULAR = new Tag('Angular', 'red');
-    static readonly CSHARP = new Tag('C#', 'green');
-    static readonly RUBY = new Tag('Ruby', 'darkred');
-    static readonly SQL = new Tag('SQL', 'turqouise');
-    static readonly JAVA = new Tag('Java', 'orange');
-    static readonly DOTNET = new Tag('.Net', 'blue');
-    static readonly Python = new Tag('Python', 'purple');
-    static readonly WINFORM = new Tag('Windows Forms', 'pink');
+    static readonly ANGULAR = new Tag('Angular', '#E01136');
+    static readonly CSHARP = new Tag('C#', '#77BD31'); 
+    static readonly RUBY = new Tag('Ruby', '#A31A0F');
+    static readonly SQL = new Tag('SQL', '#E7912E');
+    static readonly JAVA = new Tag('Java', '#56809D');
+    static readonly DOTNET = new Tag('.Net', '#AE70E8');
+    static readonly PYTHON = new Tag('Python', '#3C6A95');
+    static readonly WINFORM = new Tag('Windows Forms', '#99438B');
+    static readonly TYPESCRIPT = new Tag('Typescript', '#396FBC');
+    static readonly NODEJS = new Tag('NodeJS', '#5BBB56');
+    static readonly REACT = new Tag('React', '#0D7CA1');
+    static readonly ASPNET = new Tag('ASPnet', '#55917F');
+    static readonly JAVASCRIPT = new Tag('Javascript', '#F6B234');
+
 
 
     private constructor (private readonly key: string, public readonly color: string){
