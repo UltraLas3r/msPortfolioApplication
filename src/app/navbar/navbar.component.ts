a6f0f2bdd56b6716dac7b49e5c01c05fdbb925b1
Michael Schreiber <michaelpschreiber@gmail.com>
2023-12-26 11:39:54 -0800

    background of item selected changing colors

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
+import { concatWith } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
+
 export class NavbarComponent {
+  isNavLinkSelected= false;
+  bgColorIsChanged = false;
+  backgroundColor = '#C2CAE5';
+
   selectedNavBarItem: string | null = null;
 
-  selectedItem(navbarItemName: string){
-    this.selectedNavBarItem = navbarItemName;
+  selectedItem(item: string): void {
+    // Reset the previous item's background color
+    if (this.selectedNavBarItem) {
+      this.resetBackgroundColor();
+    }
+
+    // Set the new selected item and its background color
+    this.selectedNavBarItem = item;
+    this.backgroundColor = '#C2CAE5';
   }
 
+  resetBackgroundColor(): void {
+    // Reset the background color of the previous item
+    this.backgroundColor = '#C2CAE5';
+  }
 }
