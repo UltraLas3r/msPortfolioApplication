ce3af1ce37b2db85ebbbdab51274265e1f6f0702
Michael Schreiber <michaelpschreiber@gmail.com>
2023-12-26 17:10:07 -0800

    some changes

--- src/app/_services/projects.service.ts
+++ src/app/_services/projects.service.ts
@@ -9,12 +9,37 @@ import { Tag } from '../_models/Tag';
 export class ProjectsService {
   
   projects: Project[] = [
-    {id: 0, name: "Sample Python Project", pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], projectLink: "//www.github.com", summary: "Python project that analyzes stock market data.", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.PYTHON]},
-    {id: 1, name: "Sample Angular App", pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], projectLink: "//www.github.com", summary: "Fullstack web app developed using Angular and Node.JS", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.ANGULAR, Tag.TYPESCRIPT, Tag.NODEJS]},
-    {id: 2, name: "Sample .Net App", pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], projectLink: "//www.github.com", summary: "Fullstack web app developed using React and ASP.NET", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.REACT ,Tag.CSHARP, Tag.ASPNET]},
-    {id: 3, name: "Web API Project", pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], projectLink: "//www.github.com", summary: "Web API Project that was developed for a class project.", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.CSHARP, Tag.ASPNET]},
-    {id: 4, name: "Mobile App", pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], projectLink: "//www.github.com", summary: "Mobile app developed in java that tracks the departure and arrival of trains.", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.JAVA]},
-    {id: 5, name: "Regression Test Suite", pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], projectLink: "//www.github.com", summary: "regression testing suite", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiconsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.JAVA, Tag.SERENITY, Tag.CUCUMBER]}
+    {id: 0, name: "Inventory Management Application", 
+      pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], 
+      projectLink: "//www.github.com", 
+      summary: "A fullstack inventory management tool for organizing and sorting items and data. Uses C# for back-end design, Windows Forms for front-end design and MySql for a database service. ", 
+      description: "C# application built in Visual Studio. Uses MySql for data storage and ", 
+      tags: [Tag.CSHARP, Tag.DOTNET, Tag.SQL]},
+    {id: 1, name: "Client Scheduling Application", 
+      pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], 
+      projectLink: "//www.github.com", 
+      summary: "", 
+      description: "This complex scheduling system features a login component with user validation, access logs, and a complex date and time validation system to ensure no overlapping appointments. Uses C# and WindowsForms, as well as a MySQL database. ", 
+      tags: [Tag.ANGULAR, Tag.TYPESCRIPT, Tag.NODEJS]},
+    {id: 2, name: "Angular Website Application", 
+      pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], 
+      projectLink: "https://ms-swe-portfolio.azurewebsites.net/", 
+      summary: "Fullstack web app developed using React and ASP.NET", 
+      description: "A web application built using Angular and hosted using Microsoft Azure", 
+      tags: [Tag.REACT ,Tag.CSHARP, Tag.ASPNET]},
+    {id: 3, name: "Student Dashboard Mobile App", 
+      pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], 
+      projectLink: "//www.github.com", 
+      summary: "Web API Project that was developed for a class project.", 
+      description: "This is a Xamrin application for a student dashboard. The application allows a student to log in and manage their classes, tests, and semester schedule. The project uses C#, Xamarin/Maui and SQLite.", 
+      tags: [Tag.CSHARP, Tag.ASPNET]},
+    {id: 4, name: "Extra", 
+      pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], 
+      projectLink: "//www.github.com", 
+      summary: "Mobile app developed in java that tracks the departure and arrival of trains.", 
+      description: "Lorem  est laborum.", 
+      tags: [Tag.JAVA]},
+    //{id: 5, name: "Regression Test Suite", pictures: ["../../assets/img1.png","../../assets/img2.png","../../assets/img3.png"], projectLink: "//www.github.com", summary: "regression testing suite", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiconsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.JAVA, Tag.SERENITY, Tag.CUCUMBER]}
   ];
 
   constructor() {
